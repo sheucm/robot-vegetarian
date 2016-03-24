bb8bbe8e66ed08e1fff1f64671ba1c014415ea78
sheucm <st0502123@gmail.com>
2016-03-24 17:17:20 +0800

    complete the robot

--- robot.py
+++ robot.py
@@ -1,21 +1,79 @@
 import requests
 import dryscrape
 from bs4 import BeautifulSoup
+import json
+import re
+import sys
 
-website = 'http://clnote.tw/'
+def __is_info(p):
+	pattern = re.compile(r'(.*\n)?(.+：.+\n)+')
+	r = pattern.match(p)
+	return True if r is not None else False
 
-req = requests.get(website)
-soup = BeautifulSoup(req.text, 'html.parser')
+def __extract_info(p_all):
+	result = []
+	for p in p_all:
+		if __is_info(p.text) is True:
+			result.append(p)
+	return result
 
-articles = soup.find('div',{'id':'main', 'class':'site-main'}).find_all('article')
+def robot_vegetarianStore():
+	WEBSITE = 'http://clnote.tw/'
+	results = list()
+	req = requests.get(WEBSITE)
+	soup = BeautifulSoup(req.text, 'html.parser')
+	articles = soup.find('div',{'id':'main', 'class':'site-main'}).find_all('article')
 
 
-for article in articles:
-	title = article.h1.text
-	postTime = article.span.a.text
-	content_url = article.h1.a['href']
+	API_KEY = ''
+	with open('geocoding_apikey.txt','r') as rfile:
+		API_KEY = rfile.read().split('\n')[0]
 
-	session = dryscrape.Session()
-	session.visit(content_url)
-	content = BeautifulSoup(session.body(), 'html.parser')
-	info = content.find('div',{'class':'entry-content'}).find_all('p')[-7]
+	for idx, article in enumerate(articles):
+		title = article.h1.text
+		publishedTime = article.span.a.find('time',{'class':'entry-date published'}).text
+		content_url = article.h1.a['href']
+
+		print ("drive to article content")
+		session = dryscrape.Session()
+		session.visit(content_url)
+		content = BeautifulSoup(session.body(), 'html.parser')
+		p_all = content.find('div',{'class':'entry-content'}).find_all('p')
+
+		infos = __extract_info(p_all)
+
+		info = infos[0]  # Extract one for example if there are many infos
+		# Extract info
+		storeName = info.strong.text
+		address = info.text.split('\n')[1].split('：')[1][:-4]
+		openingTime = info.text.split('\n')[2].split('：')[1]
+		phone = info.text.split('\n')[3].split('：')[1]
+		website = info.text.split('\n')[4].split('：')[1]
+
+
+		geocoding_url = 'https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(address,API_KEY)
+		geocode = requests.get(geocoding_url)
+		jsonGeo = json.loads(geocode.text)
+		lng = jsonGeo['results'][0]['geometry']['location']['lng']
+		lat = jsonGeo['results'][0]['geometry']['location']['lat']
+
+		print ("{0}\n{1}\n{2}\n{3}\n{4},{5}\n\n".format(title,publishedTime,content_url,address,lng,lat))
+		results.append((title,storeName,publishedTime,info.text,lng,lat,content_url,website))
+		
+		if idx == 1:
+			break
+	return results
+
+
+if __name__ == '__main__':
+	timeStamp = ''
+	with open ('timeStamp.txt','r') as rfile:
+		timeStamp = rfile.read().split('\n')[0]
+	print (timeStamp)
+	results = robot_vegetarianStore()
+	print (results[0])
+	if timeStamp < results[0][2]:
+		print ('New found store')
+
+
+
