2b1a0241359115db1060a90c1c0c9af2ca4bdb21
sheucm <st0502123@gmail.com>
2016-04-14 03:24:20 +0000

    unfinished, temp store

--- mailgun.py
+++ mailgun.py
@@ -1,7 +1,7 @@
 import requests
 import robot
 def send_simple_message(subject, text):
-	with open('/home/ubuntu/documents/robot-vegetarian/emails.txt','r') as efile, \
+	with open('/home/ubuntu/documents/robot-vegetarian/test_email.txt','r') as efile, \
 	     open('/home/ubuntu/documents/robot-vegetarian/mailgun_apikey.txt','r') as apifile:
 		emails = efile.read().split('\n')[:-1]
 		apikey = apifile.read().split('\n')[0]
@@ -29,10 +29,20 @@ if __name__ == '__main__':
 			text += """文章標題：{7}\n店名：{0}\n{1}\n座標：{2},{3}\n發文時間：{4}\n網站來源：{5}\n臉書或blog:{6}\n\n"""\
 			.format(store[1],store[3],store[4],store[5],store[2],store[6],store[7],store[0])
 			newTimeStamps.append(store[2])
+
+
+			# Push data to lab api (instants)
+			data = {
+			
+		        r = requests.post('http://52.192.20.250/chat/create/robot/', data = {
+				'robot_id':'108143422899450',
+				'content':store[0],
+				'lng':store[4],
+				'lat':store[5]
+			})
+
 	if len(text) > 0:
 		send_simple_message(subject, text)
 		with open('/home/ubuntu/documents/robot-vegetarian/timeStamp.txt','w') as wfile:
 			wfile.write(max(newTimeStamps))
-
-
 
